be68d2b23fe20b2f2d399e10a6de50681d660547
eeue56 <enalicho@gmail.com>
2017-07-04 11:31:14 +0200

    add coverage for loadDir, and ensure that filtering and extension matching works as intended

--- tests/tests.js
+++ tests/tests.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 const postache = require('../index.js');
 
 
-describe("text", () => {
+describe("postache", () => {
 	it("should crash if no database was given", () => {
 		assert.throws(
 			() => postache([], [], "postgres://some.url"), 
@@ -24,5 +24,36 @@ describe("text", () => {
 	it("should not crash if auth was given", (done) => {
 		postache([], [], "postgres://username:password@some.url/omni"); 
 		done();
+	});
+});
+
+
+describe("loadDir", () => {
+	it("should only load sql files by default", (done) => {
+		const fileMap = postache.loadDir("./tests/fixtures");
+		assert.equal(Object.keys(fileMap).length, 1);
+		assert.equal(fileMap['tests/fixtures/example'], 'example');
+		done()
+	});
+
+	it("should load other files you tell it to", (done) => {
+		const fileMap = postache.loadDir("./tests/fixtures", 'json');
+		assert.equal(Object.keys(fileMap).length, 1);
+		assert.equal(fileMap['tests/fixtures/not_sql'], 'not_sql');
+		done()
+	});
+
+	it("should only match against the extension", (done) => {
+		const fileMap = postache.loadDir("./tests/fixtures", '.');
+		assert.equal(Object.keys(fileMap).length, 0);
+		done()
+	});
+
+	it("should load wildcard files you tell it to", (done) => {
+		const fileMap = postache.loadDir("./tests/fixtures", 'json|sql');
+		assert.equal(Object.keys(fileMap).length, 2);
+		assert.equal(fileMap['tests/fixtures/not_sql.json'], 'not_sql');
+		assert.equal(fileMap['tests/fixtures/example.sql'], 'example');
+		done()
 	});
 });
